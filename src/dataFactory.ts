6304e2f34ccda045f4b56da0d31ac3eb43ada0cd
okudatatsuki <okuda.yap@gmail.com>
2026-04-20 15:06:39 +0900

    122ページまで終了

--- src/dataFactory.ts
+++ src/dataFactory.ts
@@ -1,5 +1,6 @@
 import type { Menu } from "./interfaces";
 export const newMenuList: Map<number, Menu> = new Map();
+//先頭の22345はキーで、この番号でメニューを登録するってこと。数値データでなければならない
 newMenuList.set(22345, { id: 22345, name: "スパイシーチャイ", price: 700 });
 newMenuList.set(35679, { id: 35679, name: "濃いコーヒーゼリー", price: 700 });
 
